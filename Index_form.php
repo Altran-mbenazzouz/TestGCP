1730fbd523826993e7ff9e47a9ba9d07451bae8b
MarouaneBen <marouane.benazzouzs@gmail.com>
2018-12-04 14:26:41 +0100

    first commit

---
+++ Index_form.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+	<meta charset="utf-8">
+	<link rel="stylesheet" href="style.css">
+	<title> Contact form PHP</title>
+ 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+	  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+	  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
+<body>
+
+<div id="cont">
+
+</div>
+<?php
+    require 'index_formulaire.php'
+
+?>
+
+<script>
+    $( "<p>Test</p>" ).appendTo( "#cont" );
+</script>
+
+</body>
+</html>
